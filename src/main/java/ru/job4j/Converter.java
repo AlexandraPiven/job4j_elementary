fa06336d78f4f48c3e8ad6459496354402a46abb
Alexandra <apiven86@gmail.com>
2024-12-24 09:33:19 +0500

    Разбор первой программы по "взрослому" [#505151]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -8,6 +8,6 @@ public class Converter {
 
     public static double rubleToDollar(double value) {
         // Assuming the conversion rate is 1 Dollar = 75 Rubles
-        return value / 75;
+        return value / 60;
     }
 }
